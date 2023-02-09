136e915363152feefe62eb7381ce10f45c6a6956
Bianca Nilsen <biancanilsen.entra21@gmail.com>
2023-02-09 18:04:09 -0300

    alterações para deploy

--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,6 @@
 const { User } = require('../database/models');
 
 const createNewUser = async ({ email, password }) => {
-  console.log('cheguei na service')
   const userExist = await User.findOne({ where: { email } });
   if (userExist) return null;
   return await User.create({ email: email, password: password });
