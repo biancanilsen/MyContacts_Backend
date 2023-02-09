9be9728063a5095fa5335b749d7fed6bc90fdcfd
Bianca Nilsen <biancanilsen.entra21@gmail.com>
2023-02-09 16:47:35 -0300

    ajustes para deploy

--- database/config/config.js
+++ database/config/config.js
@@ -10,15 +10,6 @@ module.exports = {
         jwt_secret: process.env.JWT_SECRET,
         dialect: 'mysql',
     },
-    test: {
-        username: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        jwt_secret: process.env.JWT_SECRET,
-        dialect: 'mysql',
-    },
     production: {
         host: process.env.REMOTE_HOST,
         username: process.env.REMOTE_USERNAME,
