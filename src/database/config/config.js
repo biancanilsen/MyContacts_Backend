96738ef11a0118b507e0bcf8c944e391d7b942a8
Bianca Nilsen <biancanilsen.entra21@gmail.com>
2023-03-04 20:26:40 -0300

    remove test doteenv

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -19,13 +19,4 @@ module.exports = {
         jwt_secret: process.env.JWT_SECRET,
         dialect: 'mysql',
     },
-    test: {
-        host: process.env.TEST_DB_HOST,
-        username: process.env.TEST_DB_USER,
-        password: process.env.TEST_DB_PASS,
-        port: process.env.TEST_DB_PORT,
-        database: process.env.TEST_DB_NAME,
-        jwt_secret: process.env.JWT_SECRET,
-        dialect: 'mysql',
-    }
 };
