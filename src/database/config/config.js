de2a8feba5567a5f632e19079b6c75bc5b03fcf0
biancanilsen <biancanilsen.entra21@gmail.com>
2023-05-13 11:14:40 -0300

    Add deploy config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -18,11 +18,11 @@ module.exports = {
         synchronize: true,
     },
     production: {
-        host: process.env.REMOTE_HOST,
-        username: process.env.REMOTE_USERNAME,
-        password: process.env.REMOTE_PASSWORD,
-        port: process.env.REMOTE_PORT,
-        database: process.env.REMOTE_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         jwt_secret: process.env.JWT_SECRET,
         dialect: 'postgres',
         // dialectOptions: {
